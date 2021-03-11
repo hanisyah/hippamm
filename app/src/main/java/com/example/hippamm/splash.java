91e336b32ce9fbd99d5f4d3c52025becac77c2b0
hanisyah <hanisyahadi4@gmail.com>
2021-03-11 13:50:32 +0700

    first commit

---
+++ app/src/main/java/com/example/hippamm/splash.java
@@ -0,0 +1,31 @@
+package com.example.hippamm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class splash extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        Thread thread = new Thread() {
+            public void run() {
+                try {
+                    sleep(1500);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    Intent intent = new Intent (splash.this, login.class);
+                    startActivity(intent);
+
+                    finish();
+                }
+            }
+        };
+        thread.start();
+    }
+}
