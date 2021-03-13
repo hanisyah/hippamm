7ebde55a6d9f224ca976bd9f31ae85f034fc2ef6
hanisyah <hanisyahadi4@gmail.com>
2021-03-13 23:47:50 +0700

    scan

--- app/src/main/java/com/example/hippamm/Home.java
+++ app/src/main/java/com/example/hippamm/Home.java
@@ -2,7 +2,11 @@ package com.example.hippamm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class Home extends AppCompatActivity {
 
@@ -10,5 +14,24 @@ public class Home extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        ImageView imageInput = findViewById(R.id.imgInput);
+        TextView textInput = findViewById(R.id.txtInput);
+        ImageView imageLihat = findViewById(R.id.imgLihat);
+        TextView textLihat = findViewById(R.id.txtLihat);
+
+        imageInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), Scan.class));
+            }
+        });
+
+        textInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getBaseContext(), Scan.class));
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/example/hippamm/Scan.java
@@ -0,0 +1,49 @@
+package com.example.hippamm;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.google.zxing.Result;
+
+import me.dm7.barcodescanner.zxing.ZXingScannerView;
+
+public class Scan extends AppCompatActivity implements ZXingScannerView.ResultHandler {
+    private ZXingScannerView mScannerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scan);
+
+        mScannerView = new ZXingScannerView(this);
+        setContentView(mScannerView);
+    }
+    @Override
+    public void onResume() {
+        super.onResume();
+        mScannerView.setResultHandler(this);
+        mScannerView.startCamera();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mScannerView.stopCamera();
+    }
+
+    @Override
+    public void handleResult(Result rawResult) {
+        Log.v("TAG", rawResult.getText()); // Prints scan results
+        Log.v("TAG", rawResult.getBarcodeFormat().toString());
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Scan Result");
+        builder.setMessage(rawResult.getText());
+        AlertDialog alert1 = builder.create();
+        alert1.show();
+
+        mScannerView.resumeCameraPreview(this);
+    }
+}
