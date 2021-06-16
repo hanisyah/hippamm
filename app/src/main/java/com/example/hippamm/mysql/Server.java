2526b515f9e25615ae6ac65deab625a351f28c23
hanisyah <hanisyahadi4@gmail.com>
2021-06-16 12:06:40 +0700

    Tahun

--- app/src/main/java/com/example/hippamm/mysql/Server.java
+++ app/src/main/java/com/example/hippamm/mysql/Server.java
@@ -1,5 +1,5 @@
 package com.example.hippamm.mysql;
 
 public class Server {
-    public static final String URL = "http://192.168.1.79:8000/";
+    public static final String URL = "http://hippamtirtonurabadi.my.id/";
 }
